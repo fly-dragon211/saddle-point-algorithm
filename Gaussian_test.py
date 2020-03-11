22f51f581bc4b62ab38cf11606ec60e4e2f43ed6
fly-dragon211 <hufan_hf@163.com>
2020-03-11 10:15:15 +0800

    编写高斯测试GaussianDimer。 对dimer进行继承，使其符合分子测试程序。

---
+++ Gaussian_test.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : Gaussian_test.py
+# @Author: Fly_dragon
+# @Date  : 2020/3/10
+# @Desc  : 修改后结合高斯程序的dimer算法
+
+
+
+import numpy as np
+import os
+import re
+
+from dimer_vertical import Dimer
+
+
+class GaussianFile:
+    """
+    gaussian文件处理
+    """
+    def __init__(self):
+        self.elements = []
+        # out文件，Force比较复杂代码在下面
+        self.pattern_spe = re.compile(r'SCF Done:  E\(.+?\) =(.+?)A.U')
+        # gjf文件
+        self.pattern_coordinates = re.compile(r'-?\d\.\d{8}')
+        self.pattern_elements = re.compile(r'([A-Z][a-z]?) *?-?\d\.\d{8}')
+        self.file_head = ['%mem=500MB\n',
+                          '%nprocshared=4\n',
+                          '#p force pm6\n',
+                          '\n',
+                          'title\n',
+                          '\n',
+                          '0 1\n']
+        self.file_head_noforce = ['%mem=500MB\n',
+                                   '%nprocshared=4\n',
+                                   '#pm6\n',
+                                   '\n',
+                                   'title\n',
+                                   '\n',
+                                   '0 1\n']
+        self.file_head_noforce1 = ['%mem=500MB\n', '%nprocshared=4\n', '# B3LYP/6-31G(d) sp\n',
+                          '\n', 'title\n', '\n', '0 1\n']
+
+        self.file_rear = ['\n1 3 1.0 4 1.0 5 1.0\n', '2\n', '3\n', '4\n', '5\n']
+
+    def generate_input(self, file_path, coordinates, elements, force=False):
+        """
+        生成输入文件
+        """
+        with open(file_path, 'w+') as f:
+            if force:
+                for line in self.file_head:
+                    f.writelines(line)
+            else:
+                for line in self.file_head_noforce:
+                    f.writelines(line)
+            for i in range(len(coordinates)):
+                f.writelines(' '+elements[i]+'  '+'%.8f' % coordinates[i][0]+'  '
+                             + '%.8f' % coordinates[i][1]+'  '+'%.8f' % coordinates[i][2]+'\n')
+            # for line in self.file_rear:
+            #     f.writelines(line)
+            f.writelines('\n')
+        return
+
+    def gjf_read(self, file_path):
+        """
+        初始化读取
+        :return: 元素和坐标列表
+        """
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as f:
+                text = f.read()
+        else:
+            print('文件不存在! :', file_path)
+            return
+        self.elements = re.findall(self.pattern_elements, text)
+        coordinates = np.array(re.findall(self.pattern_coordinates, text), dtype=float)
+        coordinates = coordinates.reshape((-1, 3))
+
+        return coordinates
+
+    def _get_scf(self, file_path):
+        """
+        从out文件中得到势能
+        :param file_path:
+        :return: SCF Done energy
+        """
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as f:
+                text = f.read()
+        else:
+            print('文件不存在! :', file_path)
+            return
+        energy = re.findall(self.pattern_spe, text)
+        if len(energy) != 1:
+            print('势能不唯一')
+            return
+        return float(energy[0])
+
+    def get_force(self, positions):
+        """
+        得到受力(梯度)
+        :param positions:  x * 1 矩阵
+        :return:
+        """
+        coordinates = positions.reshape((-1, 3))
+        def _store_forces(f):
+            p = re.compile(r'-?\d\.\d{9}')
+            while True:
+                line = f.readline()
+                if re.search(r'------------', line) or (not line):
+                    break
+            forces = []
+            while True:
+                line = f.readline()
+                #         print(line)
+                if re.search(r'------------', line) or (not line):  # 这里要注意负号也为'-'
+                    break
+                r = p.findall(line)
+                forces.extend(r)
+            return forces
+
+        self.generate_input('cache.gjf', coordinates, self.elements, force=True)
+        self.run_gaussian('cache.gjf')
+        # 读取第一个forces
+        with open('cache.out', 'r', encoding='utf-8') as f:
+            forces = []
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                if re.search(r'Forces', line):
+                    forces.extend(_store_forces(f))
+                    break
+            return np.array(forces, 'f')
+
+    @staticmethod
+    def run_gaussian(input_file):
+        """
+        计算
+        """
+        print('running gaussian: ', input_file)
+        os.system('g09 '+input_file)
+        print('done')
+
+    def get_value(self, positions):
+        """
+        得到势能
+        :param positions: x * 1 矩阵
+        :return:
+        """
+        coordinates = positions.reshape((-1, 3))
+        self.generate_input('cache.gjf', coordinates, self.elements)
+        self.run_gaussian('cache.gjf')
+        energy = self._get_scf('cache.out')
+        return energy
+
+
+class DimerGaussian(Dimer):
+    def __init__(self, PES, n, ini_position=None,
+                 ini_vector=None, ini_velocity=None,
+                 whether_print=False):
+        super().__init__(PES, n, ini_position,
+                 ini_vector, ini_velocity,
+                 whether_print)
+        self.min_vertical_force = 0.05  # 旋转力收敛值
+        self.min_value = 1e-20
+        self.n = n
+        self.r = 0.005
+
+    def get_value(self, position):
+        """
+        获取值
+        """
+        return self.PES.get_value(position)
+
+    def force(self, position):
+        """
+        计算像点受力，一阶梯度
+        """
+        return self.PES.get_force(position)
+
+    def work(self):
+        self._update_all()
+        # 旋转到曲率最小
+        times = []
+        for i in range(1200):
+            pre_c = self.c
+            for j in range(200):
+                rotate_angle = self.get_rotate_angle()
+                self.rotate(rotate_angle)
+                if self.whether_print:
+                    print('rotated force: ', self.f_rota)
+                self.store_information()  # 存储dimer信息
+                # 垂直力大小
+                if np.linalg.norm(self.vertical_force(self.f1 - self.f2, self.vector)) < self.min_vertical_force:
+                    if self.c > pre_c:  # 如果曲率上升，旋转pi/2 + angle, 重要
+                        self.rotate(np.pi / 2)
+                        continue
+                    times.append(j)
+                    break
+                elif j == 199:
+                    times.append(j)
+            self.translate()
+            # 把移动后的分子存储起来
+            self.PES.generate_input('test' + str(i) + '.gjf', self.position.reshape((-1, 3)),
+                                    self.PES.elements)
+
+            if (np.abs(self.f1 + self.f2) < 0.5).all():  # 所有方向都相反
+                if self.c < 0.0:
+                    if self.whether_print:
+                        print('step: ', i)
+                    break
+        if self.whether_print:
+            print('time', times)
+            print(self.position / np.pi * 180)
+        return
+
+    def store_information(self):
+        # 存储dimer信息到 information.txt
+        with open('information.txt', 'a+') as f:
+            f.write('rotated force: ' + str(self.f_rota)
+                    + 'vector: ' + str(self.vector) + '\n')
+            f.write('curvature: ' + str(self.c))
+
+
+if __name__ == '__main__':
+    path = r'D:\graduate_project\transition_state\dimer_test_CCL'
+    os.chdir(path)
+    g = GaussianFile()
+    ini_position = g.gjf_read(r'CCL.gjf').reshape((1, -1))
+
+    np.random.seed(2)
+    d = DimerGaussian(g, ini_position.size, ini_position=ini_position)
+    d.work()
--- dimer_vertical.py
+++ dimer_vertical.py
@@ -16,27 +16,29 @@ import time
 
 
 class Dimer:
-    def __init__(self, n=2, ini_position=(np.pi/2, np.pi/2),
-                 ini_vector=np.random.rand(2)-0.5,
+    def __init__(self, PES, n=2, ini_position=None,
+                 ini_vector=None,ini_velocity=None,
                  whether_print=False):
         # 是否打印中间数据
         self.whether_print = whether_print
         # 旋转力收敛值
         self.min_vertical_force = 1e-4
-
         self.min_value = 1e-20
-        self.PES = SimpleSurface()
+        self.PES = PES
         self.n = n
-        self.r = 0.1
+        self.r = 0.005  # 半径
         self.vector = np.zeros(n, 'f')
         self.normal = np.zeros(n, 'f')
         self.position = np.zeros(n, 'f')
-        self.position[:] = ini_position[:]
         self.position_pre = np.zeros(n, 'f')
         self.f1 = np.zeros(n, 'f')
         self.f_r = np.zeros(n, 'f')  # the force of midpoint
         self.f2 = np.zeros(n, 'f')
-        self.vector[:] = ini_vector
+        self.v = np.zeros(n, 'f')  # dimer初速度
+        # 更新预设
+        self.vector[:] = ini_vector if ini_vector is not None else np.random.rand(n)-0.5
+        self.position[:] = ini_position if ini_position is not None else np.random.rand(n)
+        self.v[:] = ini_velocity if ini_velocity is not None else self.vector
         # 向量归一化
         # 这里用=/会报错
         self.vector = self.vector / np.linalg.norm(self.vector)
@@ -44,16 +46,9 @@ class Dimer:
         self.c = 0
         self.delta = 0.001
         self.delta_angle = np.pi / 180
-        # dimer速度
-        self.v = np.zeros(n, 'f')
-        # 给予一个初速度
-        # self.v[:] = self.vector
         # dimer步进时间
         self.timer = 0.06
-        self.update_f()  # 更新受力
         self.f_rota = 0  # 旋转力
-        self.update_normal()
-        self.update_c()
         self.angle = 0
         self.position_list = []
 
@@ -67,18 +62,18 @@ class Dimer:
         """
         计算像点受力，一阶梯度，这里使用移动self.delta找梯度
         """
-        value = self.get_value(position)
-        delta_array = np.zeros(self.n, 'f')
-        force_array = np.zeros(self.n, 'f')
-        for i in range(self.n):
-            delta_array[:] = 0
-            delta_array[i] = self.delta
-            new_position = position + delta_array
-            new_value = self.get_value(new_position)
-            # 负梯度为受力方向
-            force_array[i] = (value-new_value) / self.delta
-        return force_array
-        # return self.PES.get_diff(position)
+        # value = self.get_value(position)
+        # delta_array = np.zeros(self.n, 'f')
+        # force_array = np.zeros(self.n, 'f')
+        # for i in range(self.n):
+        #     delta_array[:] = 0
+        #     delta_array[i] = self.delta
+        #     new_position = position + delta_array
+        #     new_value = self.get_value(new_position)
+        #     # 负梯度为受力方向
+        #     force_array[i] = (value-new_value) / self.delta
+        # return force_array
+        return self.PES.get_diff(position)
 
     def vertical_force(self, force, vector):
         """
@@ -103,14 +98,14 @@ class Dimer:
         if f_abs < self.min_value:
             return 0
         # 垂直向量判断
-        self.update_normal()
+        self._update_normal()
         if np.isnan(self.normal[0]):
             return 0
         # 这里是单位向量
         new_vector = self.vector*np.cos(self.delta_angle) + self.normal*np.sin(self.delta_angle)
         # 微旋转后的dimer受力
         new_f1 = self.force(self.position+new_vector * self.r)
-        new_f2 = self.force(self.position-new_vector * self.r)
+        new_f2 = 2 * self.f_r - new_f1
         delta_new_f = (new_f1-new_f2) - np.dot(new_f1-new_f2, new_vector) * new_vector
         temp = f_abs*np.cos(2*self.delta_angle)-np.dot(delta_new_f, delta_f) / f_abs
         angle = np.arctan((np.sin(2*self.delta_angle)*f_abs) / temp) / 2
@@ -120,21 +115,21 @@ class Dimer:
         """
         计算dimer的旋转角
         """
-        self.update_f()
+        self._update_f()
         delta_f = (self.f1-self.f2) - np.dot(np.dot(self.f1-self.f2, self.vector), self.vector)
         # 力大小
         f_abs = np.linalg.norm(delta_f)
         if f_abs < self.min_value:
             return 0
         # 垂直向量判断
-        self.update_normal()
+        self._update_normal()
         if np.isnan(self.normal[0]):
             return 0
         # 这里是单位向量
         new_vector = self.vector*np.cos(self.delta_angle) + self.normal*np.sin(self.delta_angle)
         # 微旋转后的dimer受力
         new_f1 = self.force(self.position+new_vector * self.r)
-        new_f2 = self.force(self.position-new_vector * self.r)
+        new_f2 = 2 * self.f_r - new_f1
         delta_new_f = (new_f1-new_f2) - np.dot(np.dot(new_f1-new_f2, new_vector), new_vector)
         new_f_abs = np.linalg.norm(delta_new_f)
         if new_f_abs < self.min_value:
@@ -154,9 +149,9 @@ class Dimer:
         new_vector = self.vector*np.cos(self.angle) + self.normal*np.sin(self.angle)
         self.vector[:] = new_vector / np.linalg.norm(new_vector)
         # 更新受力
-        self.update_f()
+        self._update_f()
         # 计算曲率
-        self.update_c()
+        self._update_c()
 
     def translate(self):
         """
@@ -186,9 +181,10 @@ class Dimer:
                 self.v = delta_v * (1+np.dot(delta_v, self.v)/np.dot(delta_v, delta_v))
         self.position += (self.v * self.timer)
         # 计算新点相关数据
-        self.update_f()
+        self._update_f()
 
     def work(self):
+        self._update_all()
         # 旋转到曲率最小
         times = []
         for i in range(1200):
@@ -223,7 +219,7 @@ class Dimer:
         # self.PES.show()
         return np.array(self.position_list), times
 
-    def update_c(self):
+    def _update_c(self):
         """
         计算曲率
         """
@@ -231,7 +227,7 @@ class Dimer:
         if self.whether_print:
             print('curvature: ', self.c)
 
-    def update_normal(self):
+    def _update_normal(self):
         """
         计算法向量
         """
@@ -248,7 +244,7 @@ class Dimer:
         self.f_rota = np.linalg.norm(self.f1 - self.f2 - np.dot(self.f1 - self.f2, self.vector) * self.vector)
         return
 
-    def update_f(self):
+    def _update_f(self):
         """
         更新受力，利用像点是否移动进行效率优化
         """
@@ -262,6 +258,10 @@ class Dimer:
             self.position_pre[:] = self.position[:]
         return
 
+    def _update_all(self):
+        self._update_f()  # 更新受力
+        self._update_normal()
+        self._update_c()
 
 
 def test_1():
@@ -272,7 +272,8 @@ def test_1():
         angle = np.pi / 180 * 3
         ini_vector = [np.cos(angle), np.sin(angle)]
         ini_vector = np.random.rand(2)
-        d = Dimer(2, ini_position, ini_vector, whether_print=True)
+        PES = SimpleSurface()
+        d = Dimer(PES, 2, ini_position, ini_vector, whether_print=True)
         position_d, times_d = d.work()  # 得到dimer运行轨迹和每一次的旋转数
         d.PES.show_point_2d(position_d)
         d.PES.show_surface_2d(-5, 5)
